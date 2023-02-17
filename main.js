d3ce218b6ea579b9611b384f8f7b70fb2acb8ded
sergo321 <smuradash1@gmail.com>
2023-02-18 00:28:17 +0400

    typewriter

--- main.js
+++ main.js
@@ -11,11 +11,26 @@ const personalsec=document.querySelector(".personal")
 
 
 
+
+let i = 0;
+let txt = 'Hi, I am Sergo, I started learning programming a year ago and today I haveve already studied: HTML, CSS, JS, Angular, Github, Bootstrap, SQL,QA Testing and Postman. I am motivated by deepening existing knowledgeand acquiring new skills.'
+let speed = 70;
+function typeWriter() {
+  if (i < txt.length) {
+    document.querySelector("#typing").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
 about.addEventListener("click", ()=>{
 aboutsec.style.display="block"
+
 })
 hide1.addEventListener("click",()=>{
     aboutsec.style.display="none"
+    document.querySelector("#typing").innerHTML=""
+    i=0
 })
 
 education.addEventListener("click", ()=>{
@@ -33,17 +48,3 @@ hide3.addEventListener("click",()=>{
 
 
 
-var i = 0;
-var txt = 'Hi, Iam Sergo, I started learning programming a year ago and today
-I've already studied: HTML, CSS, JS, Angular, Github, Bootstrap, SQL,
-QA Testing and Postman. I am motivated by deepening existing knowledge
-and acquiring new skills.'
-var speed = 50;
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
